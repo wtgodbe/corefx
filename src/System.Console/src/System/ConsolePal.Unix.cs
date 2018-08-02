19121d9db5cf73fba580f1a8bbc2367d24d74e3b
wtgodbe <wigodbe@microsoft.com>
2018-08-02 15:52:55 -0700

    Use longer timeout for CPR against 2.1.2

--- src/System.Console/src/System/ConsolePal.Unix.cs
+++ src/System.Console/src/System/ConsolePal.Unix.cs
@@ -332,6 +332,20 @@ namespace System
             }
         }
 
+        /// <summary>
+        /// Tracks whether we've ever successfully received a response to a cursor position request (CPR).
+        /// If we have, then we can be more aggressive about expecting a response to subsequent requests,
+        /// e.g. using a longer timeout.
+        /// </summary>
+        private static bool s_everReceivedCursorPositionResponse;
+
+        /// <summary>
+        /// Tracks if this is out first attempt to send a cursor posotion request. If it is, we start the
+        /// timer immediately (i.e. minChar = 0), but we use a slightly longer timeout to avoid the CPR response
+        /// being written to the console.
+        /// </summary>
+        private static bool s_firstCursorPositionRequest = true;
+
         /// <summary>Gets the current cursor position.  This involves both writing to stdout and reading stdin.</summary>
         private static unsafe void GetCursorPosition(out int left, out int top)
         {
@@ -350,6 +364,15 @@ namespace System
             // just allocate, rather than employing any complicated pooling strategy.
             int readBytesPos = 0;
             Span<byte> readBytes = stackalloc byte[256];
+            int initialTimeout = 0;
+            if (s_firstCursorPositionRequest)
+            {
+                initialTimeout = 15;
+            }
+            else 
+            {
+                initialTimeout = decisecondsTimeout;
+            }
 
             // Synchronize with all other stdin readers.  We need to do this in case multiple threads are
             // trying to read/write concurrently, and to minimize the chances of resulting conflicts.
@@ -358,7 +381,15 @@ namespace System
             // one thread's get_CursorLeft/Top from providing input to the other's Console.Read*.
             lock (StdInReader) 
             {
-                Interop.Sys.InitializeConsoleBeforeRead(minChars: 0, decisecondsTimeout: 10);
+                // Because the CPR request/response protocol involves blocking until we get a certain
+                // response from the terminal, we want to avoid doing so if we don't know the terminal
+                // will definitely response.  As such, we start with minChars == 0, which causes the
+                // terminal's read timer to start immediately.  Once we've received a response for
+                // a request such that we know the terminal supports the protocol, we then specify
+                // minChars == 1.  With that, the timer won't start until the first character is
+                // received.  This makes the mechanism more reliable when there are high latencies
+                // involved in reading/writing, such as when accessing a remote system.
+                Interop.Sys.InitializeConsoleBeforeRead(minChars: (byte)(s_everReceivedCursorPositionResponse ? 1 : 0), initialTimeout: 10);
                 try
                 {
                     // Write out the cursor position report request.
@@ -429,6 +460,7 @@ namespace System
                 finally
                 {
                     Interop.Sys.UninitializeConsoleAfterRead();
+                    s_firstCursorPositionRequest = false;
                 }
 
 
